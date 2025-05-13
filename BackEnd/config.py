6889acc71e20ebae2a1302fde2e48d2325ab5dbf
Gabriel-DV <fl4usino@gmail.com>
2025-05-12 23:11:41 -0300

    Update config.py

--- BackEnd/config.py
+++ BackEnd/config.py
@@ -1,7 +1,7 @@
-# config.py
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///:memory:")  # Usando SQLite em memória para simplicidade
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "uma_chave_secreta_muito_segura") # Mantenha sua chave secreta aqui
+DATABASE = {
+    'dbname': 'saintblue',
+    'user': 'postgres',
+    'password': 'saintblue123',
+    'host': 'saintblue.cvu6ss00e5f8.us-east-1.rds.amazonaws.com',
+    'port': '5432'
+}
